2d1206c5043300002b1733a446f1649a063f248e
gbw111 <1634774713@qq.com>
2026-09-22 18:36:58 +0800

    0922

--- finetune/bridgevla/mvt/archive/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Archived experimental MVT implementations.
-
-Runtime code should import from the parent ``mvt`` package instead of this
-package.  Archived modules are retained for historical inspection and are not
-part of the current BridgeVLA execution path.
-"""
